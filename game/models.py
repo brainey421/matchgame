37101a3dc7d9d5408f197f2381ed033f9b7c75fa
brainey421 <brainey421@gmail.com>
2020-10-11 19:02:27 -0500

    Add data models for parties and prompts

--- game/models.py
+++ game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Prompt(models.Model):
+    text = models.CharField(max_length=280)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.text
+
+class Party(models.Model):
+    def __str__(self):
+        return self.id
+
+class Person(models.Model):
+    name = models.CharField(max_length=32)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
