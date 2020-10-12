219c1d5e99641c80ac2c56c291282a8fa35d4627
brainey421 <brainey421@gmail.com>
2020-10-11 23:01:45 -0500

    Show that users in the same lobby as you clicked a button

--- game/consumers.py
+++ game/consumers.py
@@ -1,2 +1,31 @@
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+
+class LobbyConsumer(WebsocketConsumer):
+    def connect(self):
+        self.party_id = self.scope['url_route']['kwargs']['party_id']
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.party_id,
+            self.channel_name
+        )
+        
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data):
+        async_to_sync(self.channel_layer.group_send)(
+            self.party_id,
+            {
+                'type': 'button_clicked',
+                'message': ''
+            }
+        )
+    
+    def button_clicked(self, event):
+        self.send(text_data=json.dumps({
+            'message': ''
+        }))
